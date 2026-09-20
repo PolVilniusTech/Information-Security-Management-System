e4b240cd17ea8f081a9680607c674a8c679ef3ad
PolVilniusTech <76515269+PolVilniusTech@users.noreply.github.com>
2026-09-20 12:20:51 +0300

    Refactor SQL query

--- src/manage.action.php
+++ src/manage.action.php
@@ -56,7 +56,7 @@ if(!$connection) {
 if(isset($_GET['discard'])) {
 	$un = (int)$_GET['discard'];
 	$checkqry = "SELECT
-				IF(EXISTS(SELECT 1 FROM corrective_action WHERE corrective_action_id = ? LIMIT 1), 1, 0) AS one;";
+				IF(EXISTS(SELECT 1 FROM corrective_action WHERE corrective_action_id = ? LIMIT 1) IS TRUE, 1, 0) AS one;";
 				
 	$chstmt = mysqli_prepare($connection, $checkqry);
 	mysqli_stmt_bind_param($chstmt,'i', $un);
@@ -118,7 +118,7 @@ if(isset($_POST['btnManage'])) {
         }
 	
 	$checkqry = "SELECT
-				IF(EXISTS(SELECT 1 FROM corrective_action WHERE corrective_action_id = ? LIMIT 1), 1, 0) AS one;";
+				IF(EXISTS(SELECT 1 FROM corrective_action WHERE corrective_action_id = ? LIMIT 1) IS TRUE, 1, 0) AS one;";
 				
 	$chstmt = mysqli_prepare($connection, $checkqry);
 	mysqli_stmt_bind_param($chstmt,'i', $un);
