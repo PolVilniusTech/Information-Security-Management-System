abd75850b83bf30f6ed785a327a54e2a3d566178
PolVilniusTech <76515269+PolVilniusTech@users.noreply.github.com>
2026-09-20 12:25:20 +0300

    Refactor SQL query

--- src/manage.riskregister.php
+++ src/manage.riskregister.php
@@ -74,7 +74,7 @@ if(!$connection) {
 if(isset($_GET['discard'])) {
 	$un = (int)$_GET['discard'];
 	$checkqry = "SELECT
-				IF(EXISTS(SELECT 1 FROM risk_register WHERE risk_un = ? LIMIT 1), 1, 0) AS one;";
+				IF(EXISTS(SELECT 1 FROM risk_register WHERE risk_un = ? LIMIT 1) IS TRUE, 1, 0) AS one;";
 				
 	$chstmt = mysqli_prepare($connection, $checkqry);
 	mysqli_stmt_bind_param($chstmt,'i', $un);
@@ -144,7 +144,7 @@ if(isset($_POST['btnManage'])) {
         }
 	
 	$checkqry = "SELECT
-				IF(EXISTS(SELECT 1 FROM risk_register WHERE risk_un = ? LIMIT 1), 1, 0) AS one;";
+				IF(EXISTS(SELECT 1 FROM risk_register WHERE risk_un = ? LIMIT 1) IS TRUE, 1, 0) AS one;";
 				
 	$chstmt = mysqli_prepare($connection, $checkqry);
 	mysqli_stmt_bind_param($chstmt,'i', $un);
